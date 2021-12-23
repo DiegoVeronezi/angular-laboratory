fa06462411734081aa8ad942db5f11699e25f8e6
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-23 15:50:13 -0300

    add(feat): dashboard module and route

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardPageComponent } from './containers/dashboard-page/dashboard-page.component';
+
+@NgModule({
+  declarations: [
+    DashboardPageComponent
+  ],
+  imports: [CommonModule, DashboardRoutingModule],
+})
+export class DashboardModule {}
