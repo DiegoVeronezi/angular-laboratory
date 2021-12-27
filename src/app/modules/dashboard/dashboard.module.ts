043db2c99ca6e52da4d04a3fbde424ff08ccd11b
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-27 10:48:45 -0300

    add(feat): implement header component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './containers/dashboard-page/dashboard-page.component';
+import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
 
 @NgModule({
-  declarations: [DashboardPageComponent],
+  declarations: [DashboardPageComponent, DashboardHeaderComponent],
   imports: [CommonModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
