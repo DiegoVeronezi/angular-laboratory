5848752993a30e14e4ff6a10213ec99ae353337d
Bruno Ramires <brunormferreira@hotmail.com>
2021-12-27 16:24:18 -0300

    add(feat): implement routes and dash nav

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardOverviewComponent } from './containers/dashboard-overview/dashboard-overview.component';
 import { DashboardPageComponent } from './containers/dashboard-page/dashboard-page.component';
+import { DashboardTransactionsComponent } from './containers/dashboard-transactions/dashboard-transactions.component';
 
-const routes: Routes = [{ path: '', component: DashboardPageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardPageComponent,
+    children: [
+      { path: 'overview', component: DashboardOverviewComponent },
+      { path: 'transactions', component: DashboardTransactionsComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
