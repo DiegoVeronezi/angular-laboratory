9320b4011c2d9f6455364b6d8ecb3638d2654085
Bruno Ramires <brunormferreira@hotmail.com>
2022-01-13 11:22:37 -0300

    add(feat): using mock on table users list and apply style to status

--- src/app/modules/dashboard/containers/dashboard-users/dashboard-users.component.ts
+++ src/app/modules/dashboard/containers/dashboard-users/dashboard-users.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-users',
   templateUrl: './dashboard-users.component.html',
   styleUrls: ['./dashboard-users.component.scss'],
 })
-export class DashboardUsersComponent implements OnInit {
-  constructor() {}
+export class DashboardUsersComponent {
+  mockArr: { status: string; name: string; email: string }[] = [
+    { status: 'Pending', name: 'John Doe', email: 'johndoe@ghost.com' },
+    { status: 'Down', name: 'Mister M', email: 'mistermm@ghost.com' },
+    { status: 'Active', name: 'Mafia', email: 'mafia@ghost.com' },
+  ];
 
-  ngOnInit(): void {}
+  getAccountStatusClass(status: string): string {
+    const classes: { [key: string]: string } = {
+      Pending: 'table__body__cell__account-status--pending',
+      Down: 'table__body__cell__account-status--down',
+      Active: 'table__body__cell__account-status--active',
+    };
+
+    return classes[status];
+  }
 }
